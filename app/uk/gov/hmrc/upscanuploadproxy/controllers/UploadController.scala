5660f8e098a011043cebb8665ba740fd6424b80b
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2019-06-05 09:37:07 +0100

    BDOG-188 Optimise upload

--- app/uk/gov/hmrc/upscanuploadproxy/controllers/UploadController.scala
+++ app/uk/gov/hmrc/upscanuploadproxy/controllers/UploadController.scala
@@ -59,86 +59,85 @@ class UploadController @Inject()(uriGenerator: UploadUriGenerator, proxyService:
 
   def upload(destination: String) =
     Action.async(rawParser) { implicit request =>
-      val url         = uriGenerator.uri(destination)
-
-      val optBoundary = request.headers.get("Content-Type")
-                          .flatMap(ct => scala.util.Try(ct.split("boundary=")(1)).toOption)
-
-      val headers     = request.headers.headers
-
+      val url = "http://localhost:9000/destination" // uriGenerator.uri(destination)
       (for {
-         boundary          <- EitherT.fromEither[Future](optBoundary
-                                .toRight(Response.badRequest("Missing boundary in headers"))
+         boundary          <- EitherT.fromEither[Future](
+                                request.headers.get("Content-Type")
+                                  .flatMap(ct => scala.util.Try(ct.split("boundary=")(1)).toOption)
+                                  .toRight(Response.badRequest("Missing boundary in headers"))
                               )
-         mat1: (Future[Either[String, Result]], Future[Context]) =
-            request.body
-            .alsoToMat(fileUpload(headers))(Keep.right)
-            .toMat(redirectUrlExtractorSink(boundary))(Keep.both)
-            .run()
-
-         (fResponse, fCtx) = mat1
-         ctx <- EitherT.liftF(fCtx)
-         _ = println(s"ctx finished: $ctx")
-         response <- EitherT(fResponse)
-                       .leftMap { b =>
-                                    println(s"failure: $b - redirecting to ${ctx.redirectUrl}")
-                                    ctx.redirectUrl match {
-                                      case Some(redirectUrl) => Response.redirect(redirectUrl, b)
-                                      case None              => Response.badRequest("Could not find error_action_redirect field in request")
-                                    }
-                                  }
-         _                 =  println("response finished")
-       } yield response.withHeaders("X-redirectUrl" -> ctx.redirectUrl.toString)
+         (fResponse, fCtx) =  request.body
+                                .alsoToMat(fileUpload(url, request.headers.headers))(Keep.right)
+                                .toMat(redirectUrlExtractorSink(boundary))(Keep.both)
+                                .run()
+         ctx               <- EitherT.liftF(fCtx)
+         redirectUrl       <- EitherT.fromEither[Future](ctx.redirectUrl
+                                .toRight(Response.badRequest("Could not find error_action_redirect field in request"))
+                              )
+         response          <- EitherT(fResponse)
+                                .leftMap(b => Response.redirect(redirectUrl, b))
+       } yield response
       ).merge
     }
 
-    class RedirectUrlHolder(var redirectUrl: Option[String] = None)
+  def toLines(v: Seq[ByteString]): Seq[String] =
+    v.map(_.utf8String).mkString.split(LineSeparator.Windows).toList
 
-    trait ReadingState
-    object ReadingState {
-      case object WaitingForRedirect  extends ReadingState
-      case object WaitingForBlankLine extends ReadingState
-      case object Reading extends ReadingState
-      case object Read    extends ReadingState
-    }
+  trait ReadingState
+  object ReadingState {
+    case object WaitingForRedirect  extends ReadingState
+    case object WaitingForBlankLine extends ReadingState
+    case object ReadingRedirect     extends ReadingState
+    case object Ignore              extends ReadingState
+  }
 
-    case class Context(state: ReadingState, redirectUrl: Option[String])
+  case class Context(state: ReadingState, redirectUrl: Option[String])
 
-    def redirectUrlExtractorSink(boundary: String): Sink[ByteString, Future[Context]] =
-      Flow[ByteString]
-        // FIXME akka.stream.scaladsl.Framing$FramingException: Read 15742 bytes which is more than 1024 without seeing a line terminator
-        //.via(Framing.delimiter(ByteString(LineSeparator.Windows), maximumFrameLength = 140000, allowTruncation = true))
-        // .recoverWith {
-        //   case e: RuntimeException => println(s"stream failure: $e"); Flow[ByteString]
-        // }
-        .sliding(2) // as long as two chunk size (default 131072?) contain a full line?
-        .mapConcat { _.map(_.utf8String).mkString.split(LineSeparator.Windows).toList}
-        .fold[Context](Context(ReadingState.WaitingForRedirect, None)){ (ctx, t) =>
-          ctx.state match {
-            case ReadingState.WaitingForRedirect if t == "Content-Disposition: form-data; name=\"error_action_redirect\""
-                                                 || t == "Content-Disposition: attachment; name=\"error_action_redirect\"" =>
-              ctx.copy(state = ReadingState.WaitingForBlankLine)
-            case ReadingState.WaitingForBlankLine if t == "" =>
-              ctx.copy(state = ReadingState.Reading)
-            case ReadingState.Reading if t == s"--$boundary" =>
-              ctx.copy(state = ReadingState.Read)
-            case ReadingState.Reading =>
-              ctx.copy(redirectUrl = ctx.redirectUrl match {
-                case None     => Some(t)
-                case Some(t1) => Some(t1 + "\n" + t)
-              })
-            case _ => ctx
+  class NoRedirectUrlException extends RuntimeException
+
+
+  def redirectUrlExtractorSink(boundary: String): Sink[ByteString, Future[Context]] =
+    Flow[ByteString]
+      .sliding(2) // as long as two chunk size (default 131072?) contain a full line?
+      .fold[Context](Context(ReadingState.WaitingForRedirect, None)){ (ctx, v) =>
+        // optimised to avoid converting to lines once we've found the redirect
+        if (ctx.state == ReadingState.Ignore) ctx
+        else
+          toLines(v).foldLeft(ctx){ (ctx, l) =>
+            ctx.state match {
+              case ReadingState.WaitingForRedirect if l == "Content-Disposition: form-data; name=\"error_action_redirect\""
+                                                    || l == "Content-Disposition: attachment; name=\"error_action_redirect\"" =>
+                ctx.copy(state = ReadingState.WaitingForBlankLine)
+              case ReadingState.WaitingForBlankLine if l == "" =>
+                ctx.copy(state = ReadingState.ReadingRedirect)
+              case ReadingState.ReadingRedirect if l == s"--$boundary" =>
+                ctx.copy(state = ReadingState.Ignore)
+              case ReadingState.ReadingRedirect =>
+                ctx.copy(redirectUrl = ctx.redirectUrl match {
+                  case None     => Some(l)
+                  case Some(l1) => Some(l1 + "\n" + l)
+                })
+              // if we hit the file, then we're not going to find the redirect url
+              case _ if (  l.startsWith("Content-Disposition: form-data; name=\"file\";")
+                        || l.startsWith("Content-Disposition: attachment; name=\"file\";")
+                        ) && ctx.redirectUrl.isEmpty =>
+                throw new NoRedirectUrlException
+              case _ => ctx
+            }
           }
-        }
-        .toMat(Sink.head)(Keep.right)
+      }
+      .recover {
+        case _: NoRedirectUrlException => Context(ReadingState.Ignore, None)
+      }
+      .toMat(Sink.head)(Keep.right)
 
-  def fileUpload(headers: Seq[(String, String)]): Sink[ByteString, Future[Either[String, Result]]] =
+  def fileUpload(url: String, headers: Seq[(String, String)]): Sink[ByteString, Future[Either[String, Result]]] =
    Sink
       .asPublisher[ByteString](fanout = false)
       .mapMaterializedValue(Source.fromPublisher)
       .mapMaterializedValue { source =>
         proxyService
-          .post("http://localhost:9000/destination", source, getRequiredHeaders(headers)).value
+          .post(url, source, getRequiredHeaders(headers)).value
       }
 
   def destination =
@@ -154,7 +153,6 @@ class UploadController @Inject()(uriGenerator: UploadUriGenerator, proxyService:
         .source[ByteString]
 	      .map(Right.apply)
   }
-
 
    //curl -i -X POST  http://localhost:9000/v1/uploads/magna-test  -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'  -F acl=public-read-write  -F Content-Type=application/text  -F key=colinssource.txt  -F error_action_redirect=https://www.amazon.co.uk  -F file=@/home/colin/Downloads/config-1.3.0.jar
 
